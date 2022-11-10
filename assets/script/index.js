6828be57eb3c0627aa92c14f1ed55b5f0295c4a3
Thelma-Dev <osifoaisosa@gmail.com>
2022-11-10 09:15:27 -0600

    Add template

---
+++ assets/script/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Utility functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function getElement(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
